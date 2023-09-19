671560c4387d3bb41d0bc032175a1c3885e971d4
notasquid1938 <shoebenfilm@gmail.com>
2023-09-19 15:37:48 -0500

    Website with working chat and broken forum

---
+++ components/banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '../styles/banner.module.css';
+
+function Banner() {
+  return (
+    <div className={styles.banner}>
+      <h1>Welcome to My Website</h1>
+      <p>Insert Clever Slogan</p>
+    </div>
+  );
+}
+
+export default Banner;
+
