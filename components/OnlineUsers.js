e50a92e78a0fe0ed65aa361b640c28dd216da089
notasquid1938 <shoebenfilm@gmail.com>
2023-10-07 15:48:06 -0500

    Finished user online count, backend works

--- components/OnlineUsers.js
+++ components/OnlineUsers.js
@@ -2,52 +2,63 @@ import React, { useState, useEffect, useRef } from 'react';
 
 function OnlineUsers() {
   const intervalRef = useRef(null); // Ref to hold the interval ID
+  const [username, setUsername] = useState(null);
 
   useEffect(() => {
-    const username = localStorage.getItem('generatedUsername');
-
-    const sendNewUserMessage = async () => {
-      try {
-        const response = await fetch('/api/usercount', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ message: 'NEW USER', username }),
-        });
-        console.log('User Joined:', username);
-
-        // Only set up the interval if it hasn't been set up already
-        if (!intervalRef.current) {
-          intervalRef.current = setInterval(async () => {
-            try {
-              const stillOnlineResponse = await fetch('/api/usercount', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ message: 'STILL ONLINE', username }),
-              });
-              console.log('Still online:', username);
-            } catch (error) {
-              console.error('Error sending "STILL ONLINE" message:', error);
-            }
-          }, 5000); // 5000 milliseconds (5 seconds)
-        }
-      } catch (error) {
-        console.error('Error sending "NEW USER" message:', error);
-      }
+    const getUsernameFromLocalStorage = () => {
+      const storedUsername = localStorage.getItem('generatedUsername');
+      setUsername(storedUsername);
     };
 
-    sendNewUserMessage();
+    getUsernameFromLocalStorage();
 
-    // Clean up the interval when the component unmounts
     return () => {
       if (intervalRef.current) {
         clearInterval(intervalRef.current);
       }
     };
   }, []);
+
+  useEffect(() => {
+    if (username) {
+      const sendNewUserMessage = async () => {
+        try {
+          const response = await fetch('/api/usercount', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ message: 'NEW USER', username }),
+          });
+          console.log('User Joined:', username);
+
+          // Only set up the interval if it hasn't been set up already
+          if (!intervalRef.current) {
+            intervalRef.current = setInterval(async () => {
+              try {
+                const stillOnlineResponse = await fetch('/api/usercount', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({ message: 'STILL ONLINE', username }),
+                });
+                console.log('Still online:', username);
+              } catch (error) {
+                console.error('Error sending "STILL ONLINE" message:', error);
+              }
+            }, 2000); // 2000 milliseconds (2 seconds)
+          }
+        } catch (error) {
+          console.error('Error sending "NEW USER" message:', error);
+        }
+      };
+
+      sendNewUserMessage();
+    }
+  }, [username]);
+
+  return null; // You may return some JSX here if needed
 }
 
 export default OnlineUsers;
--- pages/api/usercount.js
+++ pages/api/usercount.js
@@ -1,45 +1,48 @@
-let onlineUsers = new Map(); // Use a Map to store usernames and their last active timestamps
+let onlineUsers = new Map();
 
-export default async function handler(req, res) {
-  // Check for usernames that have not sent a "STILL ONLINE" message in the last 10 seconds
+function markUserAsOffline(username) {
+  onlineUsers.delete(username);
+  console.log(`${username} has been marked as offline. Online users: ${onlineUsers.size}`);
+}
+
+// Function to periodically check and mark users as offline
+function checkAndMarkUsersOffline() {
   const currentTime = Date.now();
   onlineUsers.forEach((lastActiveTime, username) => {
-    if (currentTime - lastActiveTime > 10000) {
-      onlineUsers.delete(username); // Remove the username from the set
-      console.log(`${username} has been removed from the online users.`);
+    if (currentTime - lastActiveTime > 8000) {
+      markUserAsOffline(username);
     }
   });
+}
 
+// Set up a timer to run the checkAndMarkUsersOffline function every 8 seconds
+setInterval(checkAndMarkUsersOffline, 8000);
+
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { message, username } = req.body;
 
     if (message === 'NEW USER') {
       if (!onlineUsers.has(username)) {
-        onlineUsers.set(username, Date.now()); // Store the username with its timestamp
+        onlineUsers.set(username, Date.now());
         console.log(`${username} is now online. Online users: ${onlineUsers.size}`);
-        console.log(Array.from(onlineUsers.keys())); // Convert Map keys to an array for logging
-        // Send a JSON response to the client
+        console.log(Array.from(onlineUsers.keys()));
         res.status(200).json({ message: 'User added successfully' });
       } else {
-        // User already exists in the set
         res.status(409).json({ message: 'User already exists' });
       }
-      return; // Ensure the function exits here
+      return;
     } else if (message === 'STILL ONLINE') {
-      // Handle the "STILL ONLINE" message
       if (onlineUsers.has(username)) {
-        onlineUsers.set(username, Date.now()); // Update the last active timestamp
+        onlineUsers.set(username, Date.now());
         console.log(`${username} is still online.`);
-        // You can perform additional actions here if needed.
         res.status(200).json({ message: 'User is still online' });
       } else {
-        // User is not in the onlineUsers set, handle this case if necessary.
         res.status(404).json({ message: 'User not found' });
       }
       return;
     }
 
-    // If the message is not 'NEW USER' or 'STILL ONLINE', return a 400 Bad Request
     res.status(400).json({ message: 'Bad Request' });
   } else {
     res.status(405).json({ message: 'Method Not Allowed' });
